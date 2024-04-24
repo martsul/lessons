2872f44afea3bcd99307e4d8abad861db749285d
martsul <sasham9321@mail.ru>
2024-04-24 23:04:28 +0300

    lesson-15

--- lesson-15/scripts/script.js
+++ lesson-15/scripts/script.js
@@ -1,7 +1,21 @@
-let orderForm = document.querySelector(".order")
+let orderForm = document.querySelector('.order')
+let allInputs = document.querySelectorAll('input')
 
-orderForm.addEventListener("submit", event => {
-  let textInput = orderForm.querySelectorAll("input")
-  console.log(textInput);
-  event.preventDefault();
+orderForm.addEventListener('submit', event => {
+	allInputs.forEach(element => {
+		if (element.value === '') {
+			alert('Заполните поле')
+			event.preventDefault()
+		}
+		if (
+			element.getAttribute('type' === 'tel') &&
+			!+element.value &&
+			element.value !== ''
+		) {
+			alert('Некорректный номер телефона')
+			event.preventDefault()
+		}
+    if (element.getAttribute('type' === 'radio')) {
+		}
+	})
 })
